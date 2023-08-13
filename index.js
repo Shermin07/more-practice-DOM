992b49a52e4cb1534f0eceded63d0a2bca29a62d
Shermin Sultana <rima64549@gmail.com>
2023-08-14 05:10:26 +0600

    add

---
+++ index.js
@@ -0,0 +1,42 @@
+//   ID::
+
+const listContainer = document.getElementById("list-ul");
+//console.log(listContainer);
+listContainer.style.backgroundColor = "tomato";
+listContainer.style.color = "white";
+// Class:::
+
+const list = document.getElementsByClassName("item-1")[0];
+//console.log(list);
+list.style.color = "black";
+
+// TagName:::
+
+const heading = document.getElementsByTagName("h1");
+
+heading.innerText = "hi";
+
+// QuerySelector:::
+
+const name1 = document.querySelector("#heading-two");
+name1.innerText = "Queryselector";
+name1.style.color = "red";
+//console.log(name1);
+
+//Queryselectorall:::
+
+const names1 = document.querySelectorAll(".girl")[0];
+names1.innerText = "jannat";
+names1.style.color = "red";
+//console.log(names);
+
+const names = document.querySelectorAll(".girl")
+for(const nam of names){
+    nam.style.color = "tomato";
+}
+
+
+
+
+
+
